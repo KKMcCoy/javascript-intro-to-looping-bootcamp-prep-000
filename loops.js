ad57e52296210332010740c0924030d9a40d4390
karen S <buzz.kyt@gmail.com>
2018-07-18 16:14:52 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,24 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++ ) {
+  if (i === 1) { 
+    array = [...array,"I am 1 strange loop."];
+  } else {
+    array = [...array,`I am ${i} strange loops.`];
+  }
+  
+ }return array;
+}
+
+
+function whileLoop(number) {
+  while (number >= 0) {
+    if (number > 0) {
+    console.log(--number);
+    } else {
+    return "done";
+  }
+}}
+
+function doWhileLoop {
+  
+}
